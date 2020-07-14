0b6aca9aeadcdb5f61de76f859549628d68e4518
lucasalaf <lucasalaf@gmail.com>
2020-07-14 10:52:32 -0300

    formatações nos links

--- components/Header/index.js
+++ components/Header/index.js
@@ -14,13 +14,13 @@ const Header = () => {
       </div>
       <div className='bg-gray-300 p-4 shadow-md text-center'>
         <Link href='/sobre'>
-          <a className='px-2 hover:underline'>Sobre</a>
+          <a className='px-2 hover:underline hover:text-orange-800 hover:font-bold'>Sobre</a>
         </Link>
         <Link href='/contato'>
-          <a className='px-2 hover:underline'>Contato</a>
+          <a className='px-2 hover:underline hover:text-orange-800 hover:font-bold'>Contato</a>
         </Link>
         <Link href='/pesquisa'>
-          <a className='px-2 hover:underline'>Pesquisa</a>
+          <a className='px-2 hover:underline hover:text-orange-800 hover:font-bold'>Pesquisa</a>
         </Link>
       </div>
     </React.Fragment>
